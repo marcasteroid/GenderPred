5e6150868d865081f01c437ff6695fbd2f8deb1a
Marco Margarucci <margarucci.marco@gmail.com>
2023-09-27 09:05:01 +0200

    Added UI

--- GenderPred/ContentView.swift
+++ GenderPred/ContentView.swift
@@ -7,13 +7,36 @@
 
 import SwiftUI
 
+enum Gender: String {
+    case male
+    case female
+}
+
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var predictedGender: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Text("Gender Predictor")
+                    .font(.title)
+                Image(systemName: "brain.head.profile")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+                Spacer()
+            }
+            .fontWeight(.bold)
+            .fontWidth(.standard)
+            TextField("Enter a name", text: $name)
+                .keyboardType(/*@START_MENU_TOKEN@*/.default/*@END_MENU_TOKEN@*/)
+                .textFieldStyle(.roundedBorder)
+            Text("Predicted gender")
+                .font(.headline)
+            Text(predictedGender)
+                .foregroundStyle(.red)
+            Spacer()
+            
         }
         .padding()
     }
