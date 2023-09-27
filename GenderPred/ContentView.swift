d95071ad63f80ac1835dd053611f1fd5cb4ffb9d
Marco Margarucci <margarucci.marco@gmail.com>
2023-09-27 08:44:39 +0200

    Initial Commit

---
+++ GenderPred/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenderPred
+//
+//  Created by Marco Margarucci on 27/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenderPred/GenderPredApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenderPredApp.swift
+//  GenderPred
+//
+//  Created by Marco Margarucci on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GenderPredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
